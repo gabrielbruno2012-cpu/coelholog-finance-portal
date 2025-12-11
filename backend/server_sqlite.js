2a7859b3a0ce44d32d02a785b30c31cf9287d6ca
gabrielbruno2012-cpu <gabrielbruno2012@gmail.com>
2025-12-11 14:27:52 -0300

    Update server_sqlite.js

--- backend/server_sqlite.js
+++ backend/server_sqlite.js
@@ -984,7 +984,7 @@ app.delete("/api/incentivos/pontos/excluir/:id", (req, res) => {
 // ======================================
 // START SERVER
 // ======================================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log('Server running on port', PORT);
